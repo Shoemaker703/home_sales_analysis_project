97c78fd4763bcc0789f54a399d3ee2f1b14dd091
Alexandra Marshall <89807220+AJMarshall-25@users.noreply.github.com>
2021-10-04 22:32:58 -0400

    Created GIS processing notebook and function

---
+++ notebooks/Alex M/distance_function.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from scipy.spatial import cKDTree
+from shapely.geometry import Point
+
+def ckdnearest(gdA, gdB, name):
+    '''
+    Takes in two GeoDataFrames with point geometries and finds, for every point
+    in gdA, the nearest point in gdB
+    
+    Input:
+        gdA - GeoDataFrame whose points you want to find the closest point in gdB
+        gdB - GeoDataFrame with the points you want to align with gdA's
+        name - name of column from gdB that you want included in output 
+    
+    Returns:
+        updated version of gdA with the specified column from gdB as well as the distance 
+        between that point and the row's point'''
+    
+    nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
+    nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
+    btree = cKDTree(nB)
+    
+    dist, idx = btree.query(nA, k=1)
+    
+    gdB_nearest = gdB.iloc[idx].drop(columns='geometry').reset_index(drop=True)
+    gdf = pd.concat(
+        [
+            gdA.reset_index(drop=True),
+            gdB_nearest['NAME'],
+            pd.Series(dist, name='dist')
+        ],
+        axis = 1)
+    
+    return gdf
